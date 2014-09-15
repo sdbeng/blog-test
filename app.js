9a53afcdb59a477fc24aba035cfec805ee7f445c
Sergio Barrera <sdbeng@gmail.com>
2014-09-15 11:50:08 -0700

    Fix app.js controller name declared

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var myApp = angular.module('myApp',[]);
 
-myApp.controller('DoubleController', ['$scope', function($scope) {
+myApp.controller('DoubleCtrl', ['$scope', function($scope) {
   $scope.double = function(value) { return value * 2; };
 }]);
