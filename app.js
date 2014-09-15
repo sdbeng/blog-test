0279e3c969f5ce401fac283765f04136fd25d13d
Sergio Barrera <sdbeng@gmail.com>
2014-09-15 16:39:11 -0700

    Test-5 multi-controllers

--- app.js
+++ app.js
@@ -1,28 +1,22 @@
 var myApp = angular.module('myApp',[]);
 
-myApp.controller('MailListingCtrl', ['$scope', function($scope) {
+myApp.controller('MailListingCtrl', ['$scope',$http, function($scope,$http) {
  //  $scope.double = function(value) {
  //   return (value * 2); 
 	// };
 	
 
-	$scope.email =[
-		{
-		to:"John DOe",
-		from:"Dani Cool",
-		message:"Sup John, what are you up to?"
-		},
-		{
-		to:"Mark Grand",
-		from:"Bobby Dinozzo",
-		message:"Hurry up Dino!"
-		},
-		{
-		to:"Babe Ruthy",
-		from:"Sonny Chance",
-		message:"Play that song, will ya?"
-		}
-	];
+	$scope.email = [];
+	$http({
+		method: 'GET',
+		url:'/api/data.json'
+	})
+	.success(function(data,status,headers){
+		$scope.email = data;
+	})
+	.error(function(data,status,headers){
+
+	});
 		
 
 	
