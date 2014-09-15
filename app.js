4d6214b1a7aea195273e9d7edcf68b9141a8f4e6
Sergio Barrera <sdbeng@gmail.com>
2014-09-15 13:45:10 -0700

    Test DoubleCtrl

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ var myApp = angular.module('myApp',[]);
 
 myApp.controller('DoubleCtrl', ['$scope', function($scope) {
   $scope.double = function(value) {
-   return value * 2; 
+   return (value * 2); 
 	};
 }]);
 myApp.controller('HelloCtrl', ['$scope', function($scope) {
-	$scope.name = {};
+	$scope.name = {name};
 }]);
